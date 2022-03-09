99f13678ff07ec5669609e2e58df245398cb4fdd
Mike Frager <mfrager@gmail.com>
2022-03-08 16:41:16 -0800

    Withdraw wrapped SOL in one TX

--- js/wrap_sol.js
+++ js/wrap_sol.js
@@ -1,5 +1,5 @@
 const { PublicKey, SystemProgram, Transaction } = require('@solana/web3.js')
-const { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, Token } = require('@solana/spl-token')
+const { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, AccountLayout, Token } = require('@solana/spl-token')
 const anchor = require('@project-serum/anchor')
 const { associatedTokenAddress } = require('../../js/atellix-common')
 
@@ -17,13 +17,13 @@ async function main() {
     console.log(ata.pubkey)
     let tx = new Transaction()
     let deposit = false
-    let amount = 3
+    let amount = 5
     let token = new Token(provider.connection, mint, TOKEN_PROGRAM_ID, owner)
     if (deposit) {
         try {
             await token.getAccountInfo(new PublicKey(ata.pubkey))
         } catch (error) {
-            console.log(error)
+            //console.log(error)
             tx.add(Token.createAssociatedTokenAccountInstruction(
                 ASSOCIATED_TOKEN_PROGRAM_ID,
                 TOKEN_PROGRAM_ID, 
@@ -42,40 +42,43 @@ async function main() {
             TOKEN_PROGRAM_ID, 
             new PublicKey(ata.pubkey),
         ))
+        console.log(await provider.send(tx))
     } else {
-        let account = await token.getAccountInfo(new PublicKey(ata.pubkey))
-        let balance = account.amount
-        let diff = balance.sub(new anchor.BN(amount * 10**9))
-        console.log(diff.toString())
+        let kp = anchor.web3.Keypair.generate()
+        let size = AccountLayout.span
+        let rent = await provider.connection.getMinimumBalanceForRentExemption(size)
+        tx.add(SystemProgram.createAccount({
+            fromPubkey: owner,
+            newAccountPubkey: kp.publicKey,
+            lamports: rent,
+            space: size,
+            programId: TOKEN_PROGRAM_ID,
+        }))
+        tx.add(Token.createInitAccountInstruction(
+            TOKEN_PROGRAM_ID,
+            mint,
+            kp.publicKey,
+            owner,
+        ))
+        tx.add(Token.createTransferInstruction(
+            TOKEN_PROGRAM_ID, 
+            new PublicKey(ata.pubkey),
+            kp.publicKey,
+            owner,
+            [provider.wallet.payer],
+            amount * 10**9,
+            0,
+        ))
         tx.add(Token.createCloseAccountInstruction(
             TOKEN_PROGRAM_ID, 
-            new PublicKey(ata.pubkey),
+            kp.publicKey,
             owner,
             owner,
-            []
+            [],
         ))
-        console.log(await provider.send(tx))
-        tx = new Transaction()
-        tx.add(Token.createAssociatedTokenAccountInstruction(
-            ASSOCIATED_TOKEN_PROGRAM_ID,
-            TOKEN_PROGRAM_ID, 
-            mint,
-            new PublicKey(ata.pubkey),
-            owner,
-            owner,
-        ))
-        tx.add(SystemProgram.transfer({
-            fromPubkey: owner,
-            lamports: diff.toString(),
-            toPubkey: new PublicKey(ata.pubkey),
-        }))
-        tx.add(Token.createSyncNativeInstruction(
-            TOKEN_PROGRAM_ID, 
-            new PublicKey(ata.pubkey),
-        ))
+        console.log(await provider.send(tx, [kp]))
     }
     //console.log(tx)
-    console.log(await provider.send(tx))
 }
 
 main()
